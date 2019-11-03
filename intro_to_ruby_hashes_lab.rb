923d3f795c56e8bf251654cc3fca3525aac74f38
Karl Matthes <karlmatthes@gmail.com>
2019-11-03 21:38:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,13 +27,7 @@ def monopoly_with_third_tier
   
   import_hash = monopoly_with_second_tier
   
-  import_hash[:railroads][:rental_in_dollars] = {
-    {:one_piece_owned => 25},
-    {:two_pieces_owned => 50},
-    {:three_pieces_owned => 100},
-    {:four_pieces_owned => 200}
-    
-  }
+  import_hash[:railroads][:rental_in_dollars] = {:one_piece_owned => 25}
   
   return import_hash
   
