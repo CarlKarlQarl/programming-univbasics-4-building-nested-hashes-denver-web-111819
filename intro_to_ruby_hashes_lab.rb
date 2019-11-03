c121dff4f0257ed5ec530c33bb36dc6e8eb057fd
Karl Matthes <karlmatthes@gmail.com>
2019-11-03 21:07:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,9 +35,6 @@ def monopoly_with_third_tier
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
-        :two => "two_value",
-        :three => "three_value",
-        :four => "four_value"
       }
     }
   }
