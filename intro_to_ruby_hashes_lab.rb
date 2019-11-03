71f8f3f054935928959c8e1aa221164773f3d84a
Karl Matthes <karlmatthes@gmail.com>
2019-11-03 21:25:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,22 @@ def monopoly_with_third_tier
   import_hash[:railroads][:names][:b_and_o_railroad] = {}
   import_hash[:railroads][:names][:shortline_railroad] = {}
   
-
+  {
+    :railroads => {
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      },
+      :names => {
+        :reading_railroad => {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
+      }
+    }
+  }
   
   return import_hash
   
