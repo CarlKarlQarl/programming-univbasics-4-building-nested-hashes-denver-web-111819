2045d65a86e9b7d2ca31c50fd49ec7e46076c520
Karl Matthes <karlmatthes@gmail.com>
2019-11-03 21:19:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,6 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  #{:railroads => {:pieces => 4}}
-  
   import_hash = base_hash
   
   import_hash[:railroads][:pieces] = 4
@@ -26,6 +24,8 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+  import_hash = monopoly_with_second_tier
   
   {
     :railroads => {
@@ -43,6 +43,8 @@ def monopoly_with_third_tier
       }
     }
   }
+  
+  return import_hash
   
 end
 
