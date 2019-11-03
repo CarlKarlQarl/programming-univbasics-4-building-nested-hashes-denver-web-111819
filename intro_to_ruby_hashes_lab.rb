e5af21c7280fa1423888c6aefc5ed7af0ac30e49
Karl Matthes <karlmatthes@gmail.com>
2019-11-03 21:22:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,22 +27,17 @@ def monopoly_with_third_tier
   
   import_hash = monopoly_with_second_tier
   
-  {
-    :railroads => {
-      :rent_in_dollars => {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200
-      },
-      :names => {
-        :reading_railroad => {},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
-        :shortline_railroad => {}
-      }
-    }
-  }
+  import_hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  import_hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  import_hash[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  import_hash[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  
+  import_hash[:railroads][:names][:reading_railroad] = {}
+  import_hash[:railroads][:names][:pennsylvania_railroad] = {}
+  import_hash[:railroads][:names][:b_and_o_railroad] = {}
+  import_hash[:railroads][:names][:shortline_railroad] = {}
+  
+
   
   return import_hash
   
